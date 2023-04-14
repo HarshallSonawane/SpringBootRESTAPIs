549033ad2321db37199b1739b7f249da5841509a
HarshallSonawane <harshalsonawane127@gmail.com>
2023-04-14 23:41:32 +0530

    CompleteCRUDApplications

--- src/main/java/com/api/book/bootrestbook/components/bookController.java
+++ src/main/java/com/api/book/bootrestbook/components/bookController.java
@@ -2,8 +2,11 @@ package com.api.book.bootrestbook.components;
 
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -28,7 +31,17 @@ public class bookController {
         books book = this.bookServices.addBook(b);
         System.out.println(book);
         return book;
+    }
 
+    @DeleteMapping("/books/{bookId}")
+    public void deleteBook(@PathVariable("bookId") int bId) {
+        this.bookServices.deleteBook(bId);
+    }
+
+    @PutMapping("/books/{bookId}")
+    public books updateBook(@RequestBody books b, @PathVariable("bookId") int bId) {
+        this.bookServices.updateBook(b, bId);
+        return b;
     }
 
 }
--- src/main/java/com/api/book/bootrestbook/services/bookServices.java
+++ src/main/java/com/api/book/bootrestbook/services/bookServices.java
@@ -2,6 +2,8 @@ package com.api.book.bootrestbook.services;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
+
 import org.springframework.stereotype.Service;
 import com.api.book.bootrestbook.entities.books;
 
@@ -10,6 +12,7 @@ public class bookServices {
 
     private static List<books> list = new ArrayList<>();
 
+    // DB_ITEMS
     static {
 
         list.add(new books(02, "OOPS Concepts", "Amey Dhuri"));
@@ -18,14 +21,44 @@ public class bookServices {
 
     }
 
-    public List<books> getBooks() {
-        return list;
-    }
-
-    public books addBook(books obj){
+    // CREATE
+    public books addBook(books obj) {
 
         books obj1 = obj;
         list.add(obj);
         return obj1;
     }
+
+    // READ
+    public List<books> getBooks() {
+        return list;
+    }
+
+    // UPDATE
+    public books updateBook(books book, int bId) {
+
+        list = list.stream().map(b -> {
+            if (b.getId() == bId) {
+                b.setAuthor(b.getAuthor());
+                b.setTitle(b.getTitle());
+            }
+            return b;
+        }).collect(Collectors.toList());
+
+        // for (books bk : list) {
+        // if (bk.getId() == bId) {
+        // bk.setAuthor(bk.getAuthor());
+        // bk.setTitle(bk.getTitle());
+        // }
+        // }
+        return book;
+    }
+
+    // DELETE
+    public void deleteBook(int bId) {
+
+        list = list.stream().filter(books -> books.getId() != bId).collect(Collectors.toList());
+        System.out.println("RECORD WITH ID " + bId + " DELETED");
+    }
+
 }
